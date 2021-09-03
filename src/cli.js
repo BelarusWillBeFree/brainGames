8bb218e880420816ccf5dae2d5570acbea53d6b2
Anatoli <javamuzman@gmail.com>
2021-09-03 07:57:32 +0300

    change by step three

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+const cli = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    const MessageHello = `Hello ${name}`;
+    console.log(MessageHello);
+}
+export default cli;
