597b000caa1eb11c117fb2bb2510be19b1601fbf
Anatoli <javamuzman@gmail.com>
2021-09-07 20:25:46 +0300

    fir error

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,6 @@ const cli = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   const MessageHello = `Hello ${name}`;
-  console.log(MessageHello);
-  return name;
+  return MessageHello;
 };
 export default cli;
