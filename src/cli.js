cf63a03a87a3b6ba4067e1b3b79b420dac34be7c
Anatoli <javamuzman@gmail.com>
2021-09-07 21:41:56 +0300

    fix error in message

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const cli = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  const MessageHello = `Hello ${name}`;
+  const MessageHello = `Hello, ${name}!`;
   return MessageHello;
 };
 export default cli;
