943e6722b8540c92ed96d61f56637fc8e0e5f7b8
Anatoli <javamuzman@gmail.com>
2021-09-07 05:55:29 +0300

    correct error in brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -4,22 +4,26 @@ import getRandom from '../src/getRandom.js';
 
 const brainProgression = () => {
   const generalQuestion = 'What number is missing in the progression?';
-  const Progression = [];
+  const Questions = [];
   const Answers = [];
-  let firstElement = getRandom(100);
-  const repetitionInterval = getRandom(20, 1);
-  const amountOfElements = getRandom(10, 5);
-  const hiddenElement = getRandom(amountOfElements, 1);
-  for (let i = 0; i < amountOfElements; i += 1) {
-    if ((i + 1) === hiddenElement) {
-      Answers[0] = firstElement;
-      Progression[i] = '..';
-    } else {
-      Progression[i] = firstElement;
+  for (let j = 0; j < 3; j += 1) {
+    const Progression = [];
+    let firstElement = getRandom(100);
+    const repetitionInterval = getRandom(20, 1);
+    const amountOfElements = getRandom(10, 5);
+    const hiddenElement = getRandom(amountOfElements, 1);
+    for (let i = 0; i < amountOfElements; i += 1) {
+      if ((i + 1) === hiddenElement) {
+        Answers[j] = firstElement;
+        Progression[i] = '..';
+      } else {
+        Progression[i] = firstElement;
+      }
+      firstElement += repetitionInterval;
     }
-    firstElement += repetitionInterval;
+    Questions[j] = Progression.join(', ');
   }
-  index(generalQuestion, [Progression.join(', ')], Answers);
+  index(generalQuestion, Questions, Answers);
 };
 
 brainProgression();
