421e6ed4cf81fc04ceb7e64afeaa96f6840bffde
Anatoli <javamuzman@gmail.com>
2021-09-16 18:59:12 +0300

    change README

--- src/inputNumberAndCheckEven.js
+++ src/inputNumberAndCheckEven.js
@@ -9,19 +9,19 @@ const even = () => {
   for (let numerOfRepet = 0; numerOfRepet < maxAmountQuestions; numerOfRepet += 1) {
     const numberForCheck = getRandom(maxValueRandomNumbers);
     console.log(`Question: ${numberForCheck}`);
-    const UserAnswer = readlineSync.question('Your answer: ').toLowerCase();
-    if (UserAnswer !== 'yes' && UserAnswer !== 'no') {
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    if (userAnswer !== 'yes' && userAnswer !== 'no') {
       console.log('You can only enter "yes" or "no"!');
       resultTest = false;
       break;
     }
     const isNumberEven = numberForCheck % 2 === 0;
-    if (UserAnswer === 'yes' && !isNumberEven) {
+    if (userAnswer === 'yes' && !isNumberEven) {
       console.log('"yes" is wrong answer ;(. Correct answer was "no".');
       resultTest = false;
       break;
     }
-    if (UserAnswer === 'no' && isNumberEven) {
+    if (userAnswer === 'no' && isNumberEven) {
       console.log('"no" is wrong answer ;(. Correct answer was "yes".');
       resultTest = false;
       break;
